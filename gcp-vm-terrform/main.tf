e8b6e40e06a28fe04740acaf424dcc0ddb38d66f
DevOpsWithG <sagarganesh511@gmail.com>
2025-08-11 16:46:14 +0000

    added gcp-vm-terraform

---
+++ gcp-vm-terrform/main.tf
@@ -0,0 +1,29 @@
+
+provider "google" {
+  credentials = file(var.credentials_file)
+  project = var.project_id
+  region = var.region
+  zone = var.zone
+}
+
+resource "google_compute_instance" "default"{
+  name = "devops-vm"
+  machine_type = "e2-medium"
+  zone = var.zone
+
+  boot_disk {
+    initialize_params{
+      image = "debian-cloud/debian-11"
+    }
+  }
+  
+  network_interface {
+    network = "default"
+ 
+    access_config {}
+  }
+
+  tags = ["web"]
+}
+
+
